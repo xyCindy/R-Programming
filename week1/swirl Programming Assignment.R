3dce95cf6080a8d4141861bf349775c183f78dfc
Cindy <xinyuechen213@gmail.com>
2015-10-06 15:41:38 -0400

    add swirl

---
+++ week1/swirl Programming Assignment.R
@@ -0,0 +1,15 @@
+#swirl Programming Assignment
+#0.Check R version
+R.version.string
+
+#1.install swirl
+install.packages("swirl")
+
+#2.Load and run swirl
+library(swirl)
+swirl()
+
+#3.Install the R Programming course
+install_from_swirl("R Programming")
+
+#4. Complete the lessons
